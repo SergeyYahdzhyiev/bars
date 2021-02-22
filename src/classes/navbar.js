43143fb503824a3db0a2c323ca558fb9530bace0
SergeyYahdzhyiev <mr.blooods@gmail.com>
2021-02-22 14:49:07 +0400

    develoed dynamic nav-links

--- src/classes/navbar.js
+++ src/classes/navbar.js
@@ -1,22 +1,51 @@
+import { css } from '../utils';
+
 export class Navbar {
-  constructor(id) {
+  constructor(id, styles, items) {
     this.id = id;
+    this.styles = styles;
+    this.items = items;
+  }
+
+  itemHoverHandler(event) {
+    event.target.style += css(this.styles.hover);
+  }
+
+  itemsToHTML(items = []) {
+    const html = items
+      .map((item, index) => {
+        if (index === 0) {
+          return `
+              <li class="navbar-list-item active" style="${css(
+                this.styles.item
+              )};${css(
+            this.styles.active
+          )}" onmouseover="${this.itemHoverHandler(event)}">
+                <a href="#" class="navbar-link" style="${css(
+                  this.styles.link
+                )}">${item}</a>
+              </li>`;
+        } else {
+          return `
+              <li class="navbar-list-item" style="${css(
+                this.styles.item
+              )}" onmouseover="${this.itemHoverHandler(event)}">
+                <a href="#" class="navbar-link" style="${css(
+                  this.styles.link
+                )}">${item}</a>
+              </li>`;
+        }
+      })
+      .join('');
+    return html;
   }
 
   toHTML() {
     return `
-          <nav class="navbar" id="${this.id}">
-            <div class="logo">[ LOGO ]</div>
-            <ul class="navbar-list">
-              <li class="navbar-list-item">
-                <a href="#" class="navbar-link active">Home</a>
-              </li>
-              <li class="navbar-list-item">
-                <a href="#" class="navbar-link">Contacts</a>
-              </li>
-              <li class="navbar-list-item">
-                <a href="#" class="navbar-link">About</a>
-              </li>
+          <nav class="navbar" id="${this.id}" style="${css(this.styles.nav)}">
+            <div class="logo" style="${css(this.styles.logo)}">[ LOGO ]</div>
+            <ul class="navbar-list" style="${css(this.styles.list)}">
+              ${this.itemsToHTML(this.items)}
             </ul>
           </nav>
           `;
--- src/index.js
+++ src/index.js
@@ -10,10 +10,55 @@ import './styles/main.css';
 //   burger.parentElement.classList.toggle('open');
 // }
 
+const styles = {
+  nav: {
+    display: 'flex',
+    'align-items': 'center',
+    'justify-content': 'space-between',
+    color: 'white',
+    'background-color': 'rgb(0, 0, 0)',
+    height: '10vh',
+    overflow: 'hidden',
+  },
+  logo: {
+    'font-size': '24px',
+    'letter-spacing': '2px',
+    'padding-left': '1.5em',
+  },
+  list: {
+    display: 'flex',
+    'justify-content': 'space-around',
+    padding: '0 1em',
+    height: '100%',
+  },
+  item: {
+    display: 'flex',
+    transition: 'background-color 200ms',
+  },
+  link: {
+    'flex-direction': 'column',
+    'align-items': 'center',
+    'justify-content': 'center',
+    display: 'flex',
+    padding: '0 1.5em',
+    'font-size': '16px',
+    'text-decoration': 'none',
+    color: 'white',
+  },
+  hover: {
+    'background-color': 'gray',
+  },
+  active: {
+    'background-color': 'gray',
+  },
+};
+
+const items = ['Home', 'Contacts', 'About'];
+
 const container = document.querySelector('.navbar-container');
 
-const navbar = new Navbar('nav-1').toHTML();
+const navbar = new Navbar('nav-1', styles, items).toHTML();
 
 container.innerHTML = navbar;
 
-const $navbar = document.getElementById('nav-1');
+console.log(new Navbar('nav-1', styles, items).itemsToHTML());
