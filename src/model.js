25313e43e67eeeccc2ab9ecda6e3d3bddd2fb22b
SergeyYahdzhyiev <mr.blooods@gmail.com>
2021-04-12 23:14:16 +0300

    added light navbar

--- src/model.js
+++ src/model.js
@@ -46,4 +46,49 @@ export const model = [
     },
     items: ['Home', 'Contacts', 'About'],
   }),
+  new Section({
+    id: 'nav-2',
+    title: 'Simple Light',
+    type: 'simple',
+    styles: {
+      nav: {
+        display: 'flex',
+        'justify-content': 'space-between',
+        'align-items': 'center',
+        color: 'black',
+        'background-color': 'rgb(255,255,255)',
+        height: '10vh',
+        margin: '1em 0',
+      },
+      logo: {
+        'font-size': '24px',
+        'letter-spacing': '2px',
+        'padding-left': '1.5em',
+      },
+      list: {
+        display: 'flex',
+        'justify-content': 'space-around',
+        padding: '0 1em',
+        height: '100%',
+      },
+      item: {
+        display: 'flex',
+        transition: 'background-color 200ms',
+      },
+      link: {
+        'flex-direction': 'column',
+        'align-items': 'center',
+        'justify-content': 'center',
+        display: 'flex',
+        padding: '0 1.5em',
+        'font-size': '16px',
+        'text-decoration': 'none',
+        color: 'black',
+      },
+      hover: {
+        'background-color': 'lightgray',
+      },
+    },
+    items: ['Home', 'Contacts', 'About'],
+  }),
 ];
